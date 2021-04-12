0fb6c5579d2923079ca03efa6baf3c273bdfff0b
Lokendar Singh Rathore <67568211+rathodloki@users.noreply.github.com>
2021-04-12 11:07:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello pipeline webhook'));
+app.get('/', (req, res) => res.send('Hello pipeline webhook jkbj'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
